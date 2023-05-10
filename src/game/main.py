b930e0113c8cd157f0f39b179ceb65bcf77a6f55
fmaachadoo <filipe__machado@hotmail.com>
2023-05-10 02:56:14 -0300

    feat: add animation for the search algorithm
    Co-authored-by: freitasc <caina2000@gmail.com>

--- src/game/main.py
+++ src/game/main.py
@@ -13,6 +13,9 @@ class Player:
         self.color = (0, 255, 0)
         self.size = (grid.CELL_SIZE, grid.CELL_SIZE)
 
+    def __getitem__(self, item):
+        return self.position[item]
+
     def draw(self, screen):
         rect = pygame.Rect(
             self.grid.GRID_POSITION[0] + (self.position[0] * self.grid.CELL_SIZE),
@@ -27,6 +30,61 @@ class Player:
             width=0,
             border_radius=0
         )
+
+
+class AStar:
+    def __init__(self, grid, start, goal):
+        self.grid = grid
+        self.start = start
+        self.goal = goal
+
+        self.open_set = {start}
+        self.came_from = {start: None}
+        self.g_score = {start: 0}
+        self.f_score = {start: self.heuristic(start, goal)}
+        self.explored = set()
+
+    def heuristic(self, cell, goal):
+        # Manhattan distance
+        return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
+
+    def get_neighbors(self, cell):
+        # Returns walkable neighbors
+        neighbors = []
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = cell[0] + dx, cell[1] + dy
+            if (0 <= nx < self.grid.width and 0 <= ny < self.grid.height and
+                    self.grid.CELL_TYPE[self.grid.grid[ny][nx]]['walkable']):
+                neighbors.append((nx, ny))
+        return neighbors
+
+    def a_star_tick(self):
+        if not self.open_set:
+            return None, self.explored  # No path found
+
+        current = min(self.open_set, key=lambda cell: self.f_score[cell])
+        self.explored.add(current)  # add current cell to explored set
+
+        if current == self.goal:
+            path = []
+            while current is not None:
+                path.append(current)
+                current = self.came_from[current]
+            path.reverse()
+            return path, self.explored
+
+        self.open_set.remove(current)
+
+        for neighbor in self.get_neighbors(current):
+            tentative_g_score = self.g_score[current] + self.grid.CELL_TYPE[self.grid.grid[neighbor[1]][neighbor[0]]]['cost']
+            if neighbor not in self.g_score or tentative_g_score < self.g_score[neighbor]:
+                self.came_from[neighbor] = current
+                self.g_score[neighbor] = tentative_g_score
+                self.f_score[neighbor] = tentative_g_score + self.heuristic(neighbor, self.goal)
+                if neighbor not in self.open_set:
+                    self.open_set.add(neighbor)
+
+        return None, self.explored  # Path not yet found
 
 
 class Grid:
@@ -58,7 +116,8 @@ class Grid:
     GRID_POSITION = (CELL_SIZE  * 2, CELL_SIZE * 2)
 
     path = []
-    
+
+    explored = []
 
     def __init__(self, width, height):
         self.width = width
@@ -73,59 +132,20 @@ class Grid:
                 if random.randint(0, 100) < 20:
                     self.grid[y][x] = 1
 
-    def get_neighbors(self, cell):
-        neighbors = []
-        for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:  # 4-connectivity
-            x, y = cell[0] + dx, cell[1] + dy
-            if 0 <= x < self.width and 0 <= y < self.height and self.CELL_TYPE[self.grid[y][x]]['walkable']:
-                neighbors.append((x, y))
-        return neighbors
-
-    def heuristic(self, cell1, cell2):
-        return abs(cell1[0] - cell2[0]) + abs(cell1[1] - cell2[1])  # Manhattan distance
-
-
-    def a_star(self, start, goal):
-        open_set = {start}
-        came_from = {start: None}
-        g_score = {start: 0}
-        f_score = {start: self.heuristic(start, goal)}
-
-        while open_set:
-            current = min(open_set, key=lambda cell: f_score[cell])
-
-            if current == goal:
-                path = []
-                while current is not None:
-                    path.append(current)
-                    current = came_from[current]
-                path.reverse()
-                self.path = path
-                return
-
-            open_set.remove(current)
-
-            for neighbor in self.get_neighbors(current):
-                tentative_g_score = g_score[current] + self.CELL_TYPE[self.grid[neighbor[1]][neighbor[0]]]['cost']
-                if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
-                    came_from[neighbor] = current
-                    g_score[neighbor] = tentative_g_score
-                    f_score[neighbor] = tentative_g_score + self.heuristic(neighbor, goal)
-                    if neighbor not in open_set:
-                        open_set.add(neighbor)
-
     def draw_grid(self, screen):
         for y in range(self.height):
             for x in range(self.width):
                 rect = pygame.Rect(
                     self.GRID_POSITION[0] + (x * self.CELL_SIZE),
                     self.GRID_POSITION[1] + (y * self.CELL_SIZE),
-                    self.CELL_SIZE, 
+                    self.CELL_SIZE,
                     self.CELL_SIZE
                 )
 
                 if (x, y) in self.path:
                     cell_color = (128, 128, 128)
+                if (x, y) in self.explored:
+                    cell_color = (0, 0, 255)
                 else:
                     cell_color = self.CELL_TYPE[self.grid[y][x]]['color']
 
@@ -154,9 +174,13 @@ if __name__ == '__main__':
     from pprint import pprint
     # pprint(grid.grid)
 
+
+    # Create the player
     player = Player(grid)
 
-    grid.a_star(player.position, (10, 20))
+
+    goal = (10,20)
+    solver = AStar(grid, player, goal)
 
     pprint(grid.path)
 
@@ -167,6 +191,18 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+        # Perform a step of the A* algorithm
+        path, explored = solver.a_star_tick()
+
+        # If a path was found, store it
+        if path is not None:
+            grid.path = path
+        else:
+            print('Impossible path')
+
+        # Store the explored cells
+        grid.explored = explored
 
         # Fill the window with red
         window.fill((0, 0, 0))
