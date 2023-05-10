f79d0121c0e3410c6f30499177f1667a494274da
fmaachadoo <filipe__machado@hotmail.com>
2023-05-10 02:17:41 -0300

    feat: add a* algorithm
    Co-authored-by: freitasc <caina2000@gmail.com>

--- src/game/main.py
+++ src/game/main.py
@@ -3,11 +3,13 @@ import pygame
 import sys
 import random
 
+
+
 class Player:
 
     def __init__(self, grid):
         self.grid = grid
-        self.position = [0, 0]
+        self.position = (0, 0)
         self.color = (0, 255, 0)
         self.size = (grid.CELL_SIZE, grid.CELL_SIZE)
 
@@ -35,16 +37,27 @@ class Grid:
         0: {
             'name': 'floor',
             'color': (255, 255, 255),
-            'walkable': True
+            'walkable': True,
+            'cost': 1
         },
         1: {
             'name': 'wall',
             'color': (255, 0, 0),
-            'walkable': False
+            'walkable': False,
+            'cost': 0
+        },
+        2: {
+            'name': 'lava',
+            'color': (255, 255, 0),
+            'walkable': True,
+            'cost': 10
         }
+
     }
 
     GRID_POSITION = (CELL_SIZE  * 2, CELL_SIZE * 2)
+
+    path = []
     
 
     def __init__(self, width, height):
@@ -60,6 +73,47 @@ class Grid:
                 if random.randint(0, 100) < 20:
                     self.grid[y][x] = 1
 
+    def get_neighbors(self, cell):
+        neighbors = []
+        for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:  # 4-connectivity
+            x, y = cell[0] + dx, cell[1] + dy
+            if 0 <= x < self.width and 0 <= y < self.height and self.CELL_TYPE[self.grid[y][x]]['walkable']:
+                neighbors.append((x, y))
+        return neighbors
+
+    def heuristic(self, cell1, cell2):
+        return abs(cell1[0] - cell2[0]) + abs(cell1[1] - cell2[1])  # Manhattan distance
+
+
+    def a_star(self, start, goal):
+        open_set = {start}
+        came_from = {start: None}
+        g_score = {start: 0}
+        f_score = {start: self.heuristic(start, goal)}
+
+        while open_set:
+            current = min(open_set, key=lambda cell: f_score[cell])
+
+            if current == goal:
+                path = []
+                while current is not None:
+                    path.append(current)
+                    current = came_from[current]
+                path.reverse()
+                self.path = path
+                return
+
+            open_set.remove(current)
+
+            for neighbor in self.get_neighbors(current):
+                tentative_g_score = g_score[current] + self.CELL_TYPE[self.grid[neighbor[1]][neighbor[0]]]['cost']
+                if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_g_score
+                    f_score[neighbor] = tentative_g_score + self.heuristic(neighbor, goal)
+                    if neighbor not in open_set:
+                        open_set.add(neighbor)
+
     def draw_grid(self, screen):
         for y in range(self.height):
             for x in range(self.width):
@@ -69,7 +123,12 @@ class Grid:
                     self.CELL_SIZE, 
                     self.CELL_SIZE
                 )
-                cell_color = self.CELL_TYPE[self.grid[y][x]]['color']
+
+                if (x, y) in self.path:
+                    cell_color = (128, 128, 128)
+                else:
+                    cell_color = self.CELL_TYPE[self.grid[y][x]]['color']
+
                 pygame.draw.rect(
                     surface=screen,
                     color=cell_color,
@@ -92,9 +151,14 @@ if __name__ == '__main__':
 
     # Create the grid
     grid = Grid(24, 32)
-    from pprint import pprint; pprint(grid.grid)
+    from pprint import pprint
+    # pprint(grid.grid)
 
     player = Player(grid)
+
+    grid.a_star(player.position, (10, 20))
+
+    pprint(grid.path)
 
     # Game loop
     while True:
