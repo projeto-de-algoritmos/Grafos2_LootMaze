5ca55f2f00e0efdc593a6272cff32bf55ffe6aa5
fmaachadoo <filipe__machado@hotmail.com>
2023-05-09 22:13:37 -0300

    feat: initialize pygame

--- src/game/main.py
+++ src/game/main.py
@@ -1,4 +1,25 @@
 from src.config import WINDOW_WIDTH, WINDOW_HEIGHT
+import pygame
+import sys
+
 
 if __name__ == '__main__':
-    print(f'Window: {WINDOW_WIDTH}, {WINDOW_HEIGHT}')
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up the game window
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    # Game loop
+    while True:
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Fill the window with red
+        window.fill((255, 0, 0))
+
+        # Update the display
+        pygame.display.flip()
