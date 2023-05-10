c75a06e9cb61ba28a993121165192cde0ea6d36b
fmaachadoo <filipe__machado@hotmail.com>
2023-05-09 22:05:02 -0300

    chore: fix python setup

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	python -m pip install -r requirements.txt
+
+run: 
+	python -m src.game.main
---
+++ src/config/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+WINDOW_WIDTH = os.environ.get('WINDOW_WIDTH', 800)
+WINDOW_HEIGHT = os.environ.get('WINDOW_HEIGHT', 600)
