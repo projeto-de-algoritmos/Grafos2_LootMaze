9442c736af41a3e5ca0c491d4e02408f731b4ba2
fmaachadoo <filipe__machado@hotmail.com>
2023-05-15 18:46:07 -0300

    feat: add menu
    Co-authored-by: freitasc <caina2000@gmail.com>

--- src/game/menu/__init__.py
+++ src/game/menu/__init__.py
@@ -1,23 +1,38 @@
+import pygame
 
 
 class Menu:
     def __init__(self, screen):
         self.screen = screen
         self.running = True
+        self.start_button = pygame.Rect(0, 0, 200, 80)
+        self.quit_button = pygame.Rect(0, 100, 200, 80)
+        self.game_scene = False
 
     def handle_event(self, event):
         if event.type == pygame.QUIT:
             self.running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Check if the click happened on your menu buttons
-            pass  # Replace this with your button click handling code
+            if self.quit_button.collidepoint(event.pos):
+                self.running = False
+                self.game_scene = False
+            elif self.start_button.collidepoint(event.pos):
+                self.running = False
+                self.game_scene = True
 
     def run(self):
         while self.running:
             for event in pygame.event.get():
                 self.handle_event(event)
+
             # Draw everything for the menu
             self.screen.fill((0,0,0))
-            # Replace this with your drawing code
+            
+            # Draw start button
+            pygame.draw.rect(self.screen, (255, 0, 0), self.start_button)
+            # Draw quit button
+            pygame.draw.rect(self.screen, (255, 0, 0), self.quit_button)
+            
             pygame.display.flip()
-        return self.running
+        
+        return self.running, self.game_scene
